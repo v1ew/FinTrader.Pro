dbd62e8e03220a8ec6a1e62564be4d21735212ca
Vitaliy Shakhov <italick@mail.ru>
2021-04-11 01:40:28 +0300

    Added @angular/material and managed start front

--- FinTrader.Pro.Web/Startup.cs
+++ FinTrader.Pro.Web/Startup.cs
@@ -70,10 +70,17 @@ namespace FinTrader.Pro.Web
                     pattern: "{controller=Home}/{action=Index}/{id?}");
             });
 
-            app.UseSpa(spa =>
-            {
-                spa.Options.SourcePath = "../ClientApp";
-                spa.UseAngularCliServer("start");
+            app.UseSpa(spa => {
+                string strategy = Configuration.GetValue<string>("DevTools:ConnectionStrategy");
+                if (strategy == "proxy")
+                {
+                    spa.UseProxyToSpaDevelopmentServer("http://127.0.0.1:4200");
+                }
+                else if (strategy == "managed")
+                {
+                    spa.Options.SourcePath = "../ClientApp";
+                    spa.UseAngularCliServer("start");
+                }
             });
         }
     }

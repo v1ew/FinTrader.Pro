42ba56b832e9661678a54dd99bb87524affed44f
Vitaliy Shakhov <italick@mail.ru>
2021-08-18 06:17:40 +0300

    Added changing limits for slider

--- FinTrader.Pro.Web/ClientApp/src/app/bonds-picker/bonds-picker-form/bonds-picker-form.component.ts
+++ FinTrader.Pro.Web/ClientApp/src/app/bonds-picker/bonds-picker-form/bonds-picker-form.component.ts
@@ -15,6 +15,7 @@ interface SelectItem {
 })
 export class BondsPickerFormComponent implements OnInit {
   public bondsPicker: BondsPickerFilter;
+
   public bondClasses: SelectItem[] = [
     { value: "MostLiquid", text: "Самые ликвидные" },
     { value: "FarthestRepaynment", text: "По самому дальнему погашению" },
@@ -29,7 +30,7 @@ export class BondsPickerFormComponent implements OnInit {
 
   constructor(private router: Router, private repo: Repository) {
     if (this.repo.savedFilter == null) {
-      this.bondsPicker = new BondsPickerFilter(false, false, "MostLiquid", null, false, "InvestmentAmount", 10000, false, false, false);
+      this.bondsPicker = new BondsPickerFilter(false, false, "MostLiquid", null, false, "InvestmentAmount", this.sliderMin, false, false, false);
     } else {
       this.bondsPicker = this.repo.savedFilter;
     }
@@ -58,6 +59,18 @@ export class BondsPickerFormComponent implements OnInit {
     this.bondsPicker.isOneBondByIssuer = false;
   }
 
+  onToggleIsTwoPortfolios(): void {
+    this.checkAmount();
+  }
+
+  checkAmount(): void {
+    if (this.bondsPicker.isTwoPortfolios
+      && this.bondsPicker.calculationMethod == 'InvestmentAmount'
+      && this.bondsPicker.amount < this.sliderMin) {
+      this.bondsPicker.amount = this.sliderMin;
+    }
+  }
+
   formatLabel(value: number) {
     if (value >= 1000000) {
       return Math.round(value / 10000) / 100 + 'm';
@@ -67,5 +80,13 @@ export class BondsPickerFormComponent implements OnInit {
     }
 
     return value;
+  }
+
+  get sliderMin(): number {
+    return this.bondsPicker?.isTwoPortfolios && this.bondsPicker?.calculationMethod == 'InvestmentAmount' ? 20000 : 10000;
+  }
+
+  get sliderMax(): number {
+    return this.bondsPicker?.calculationMethod == 'MonthlyCoupon' ? 200000 : 5000000;
   }
 }
